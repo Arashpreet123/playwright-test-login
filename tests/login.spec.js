1530adf25041b02469c3dc3574afd904f400ace8
Arashpreet123 <pandherarashpreet@gmail.com>
2026-02-09 20:25:58 -0800

    Updated login.spec.js file. Completed "Valid Login" test. Added new test for invalid login.

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,5 +5,27 @@ test("Valid Login", async function({page}) {
    
     await page.goto("https://practicetestautomation.com/practice-test-login/")
 
+    await page.locator('#username').fill('student');
+
+    await page.locator('#password').fill('Password123');
+
+    await page.locator('#submit').click();
+
+    await expect(page.locator('h1')).toHaveText('Logged In Successfully');
+    await expect(page).toHaveURL(/logged-in-successfully/);
+
     // await page.getBy
 }) 
+
+
+// Testing invalid login
+test("Invalid Login", async ({ page }) => {
+  await page.goto("https://practicetestautomation.com/practice-test-login/");
+
+  await page.fill('#username', 'wrongUser');
+  await page.fill('#password', 'wrongPass');
+  await page.click('#submit');
+
+  await expect(page.locator('#error')).toContainText('Your username is invalid!');
+});
+
